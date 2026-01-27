8261552ecdcb9f4e002e4a3192c16cd884a4953a
Issac Newton <newton.issac.1642.1727@gmail.com>
2026-01-28 00:07:48 +0900

    Add DevOps tools installation to startup script

--- startup.sh
+++ startup.sh
@@ -40,3 +40,11 @@ pip3 install -r requirements.txt  --break-system-packages
 
 # === Install Bun packages ===
 bun install -g prettier
+
+# === DevOps tools ===
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv kubectl ~/.local/bin
